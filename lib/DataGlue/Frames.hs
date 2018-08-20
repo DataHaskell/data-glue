d288d22906fbd44961593210faf8c714857967f9
Damien Leprovost <damien.leprovost@tweag.io>
2018-08-20 11:00:25 +0200

    display Frame of Int/Double/Text.

--- lib/DataGlue/Frames.hs
+++ lib/DataGlue/Frames.hs
@@ -31,32 +31,66 @@ type FRecord ts = (AsVinyl ts, ColumnHeaders ts, RecAll Identity ts Show
     , RecAll Identity (UnColumn ts) Show, RecVec ts)
 
 instance FRecord ts => D.IHaskellDisplay (Frame (Record ts)) where
-  display = return . prettyFrame
+  display = return . prettyRecFrame
+
+instance D.IHaskellDisplay (Frame Int) where
+  display = return . prettyValueFrame
+
+instance D.IHaskellDisplay (Frame Double) where
+  display = return . prettyValueFrame
+
+instance D.IHaskellDisplay (Frame Text) where
+  display = return . prettyValueFrame
 
 instance FRecord ts => D.IHaskellDisplay (Record ts) where
   display = return . prettyRecord
 
-prettyFrame :: FRecord ts => Frame (Record ts) -> D.Display
-prettyFrame df@(Frame fLen _) =
-    D.Display [D.html $ prettyTable headers prettydf]
+prettyRecFrame :: FRecord ts => Frame (Record ts) -> D.Display
+prettyRecFrame df =
+    D.Display [D.html $ prettyTable $ (prettyHRow headers) ++ prettyFrame]
   where
     headers = columnHeaders df
-    prettydf
-      | fLen < 20 = prettyPartofTable df
-      | otherwise = prettyPartofTable (takeFrameRow 10 df)
-          ++ "<tr><td style='text-align:center' colspan="
-          ++ (show $ L.length headers) ++">. . .</td></tr>"
-          ++ prettyPartofTable (dropFrameRow (fLen-10) df)
-    prettyTable = (("<table>" ++) .) . (. (++ "</table>")) . (++) . prettyHRow
-    prettyPartofTable = foldMap prettyRow
-    prettyRow = (++ "</tr>") . foldl' ((. prettyCell) . (++)) "<tr>"
-        . showFields
-    prettyCell = ("<td>" ++) . (++ "</td>")
-    prettyHRow = (++ "</tr>") . foldl' ((. prettyHCell) . (++)) "<tr>"
-    prettyHCell = ("<th>" ++) . (++ "</th>")
+    prettyFrame = sampledFrame df f (L.length $ columnHeaders df)
+    f = foldMap (prettyRow . showFields)
+
+prettyValueFrame :: Show a => Frame a -> D.Display
+prettyValueFrame df = D.Display [D.html $ prettyTable $ prettyFrame]
+  where
+    prettyFrame = sampledFrame df f 1
+    f = foldMap (prettyRow . return . show)
+
+sampledFrame :: Frame r -> (Frame r -> String) -> Int -> String
+sampledFrame df@(Frame fLen _) f width
+    | fLen < 20 = f df
+    | otherwise = f (takeFrameRow 10 df)
+        ++ prettySampleSeparator width
+        ++ f (dropFrameRow (fLen-10) df)
 
 prettyRecord :: FRecord ts => Record ts -> D.Display
-prettyRecord rec = prettyFrame $ toFrame [rec]
+prettyRecord rec = prettyRecFrame $ toFrame [rec]
+
+prettySampleSeparator :: (Num a, Show a) => a -> String
+prettySampleSeparator n =
+  "<tr><td style='text-align:center' colspan=" ++ show n ++">. . .</td></tr>"
+
+prettyRow, prettyHRow :: [String] -> String
+prettyRow = prettyRow' prettyCell
+prettyHRow = prettyRow' prettyHCell
+
+prettyRow' :: (String -> String) -> [String] -> String
+prettyRow' f = innerHtml "tr" . foldl' ((. f) . (++)) ""
+
+prettyTable, prettyCell, prettyHCell :: String -> String
+prettyTable = innerHtml "table"
+prettyCell = innerHtml "td"
+prettyHCell = innerHtml "th"
+
+innerHtml :: String -> String -> String
+innerHtml tag = (inHtml tag ++) . (++ outHtml tag)
+
+inHtml, outHtml :: String -> String
+inHtml = ("<" ++) . (++ ">")
+outHtml = ("</" ++) . (++ ">")
 
 -- XXX: Do `describe` (like in Python)
 -- XXX: Do `str` (like in R)
